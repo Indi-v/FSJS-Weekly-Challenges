0901cb6c721849a7f3b34a99c65c9e7718dfa41c
Travis Davis <d.travis.davis@gmail.com>
2016-09-12 23:01:37 -0400

    --Changed button to ID. --Centered the flash card area. --Added a few comments on my Java script.

--- Challenges/Week3/js/app.js
+++ Challenges/Week3/js/app.js
@@ -18,11 +18,13 @@ $('.flash-card').mouseover(function(){
 }).blur(function(){$(this).mouseleave()});
 
 /*  4.) Use jQuery to find the button element on the page and have it toggle all of the answers on and off when clicked.
-    Hint: jQuery has a toggle function that can toggle the visibility of a selected DOM node.
-    Bonus:  Change the text of the button using jQuery when you toggle the answers on/off. */
-$(".cheat-button").click(function(){
+    Hint: jQuery has a toggle function that can toggle the visibility of a selected DOM node. */
+
+$("#cheat-button").click(function(){
   $('.answer').toggle();
+  // my boolean toggle if false then not false on click.
   binary = !binary
+  //  Bonus:  Change the text of the button using jQuery when you toggle the answers on/off.
   if (binary) {
     $(this).text("Hide All Answers")
   } else {
